20d8b5d44603927b80c19e06f17245d131e82b89
M-Abeer <102969595+M-Abeer@users.noreply.github.com>
2024-01-18 00:03:54 +0500

    React Greeting Project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+let date = new Date().getHours();
+let greet;
+let msg;
+if (date >= 1 && date <= 11) {
+  greet = " Morning";
+  msg = "green";
+} else if (date >= 12 && date <= 19) {
+  greet = " Evening";
+  msg = "orange";
+} else if (date > 19 && date <= 24) {
+  greet = " Night";
+  msg = "black";
+}
+
+function App() {
+  return (
+    <div className="primary">
+      <h1 className="a">
+        Hello sir, <span style={{ color: msg }}> Good {greet}</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
