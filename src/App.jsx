31f0c560db83371f9da00cb103253b48ebe6b591
M-Abeer <102969595+M-Abeer@users.noreply.github.com>
2024-01-18 23:07:16 +0500

    Updated
    Use Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-let date = new Date().getHours();
-let greet;
-let msg;
-if (date >= 1 && date <= 11) {
-  greet = " Morning";
-  msg = "green";
-} else if (date >= 12 && date <= 19) {
-  greet = " Evening";
-  msg = "orange";
-} else if (date > 19 && date <= 24) {
-  greet = " Night";
-  msg = "black";
-}
+import React from "react";
+import "./style.css";
+import Text from "./Text";
 
 function App() {
-  return (
-    <div className="primary">
-      <h1 className="a">
-        Hello sir, <span style={{ color: msg }}> Good {greet}</span>
-      </h1>
-    </div>
-  );
+  return <Text />;
 }
 
 export default App;
